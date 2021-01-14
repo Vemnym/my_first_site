da4c41c4b4154144209d4b37f6a2b982f5828b79
vemnym <mamaevk52@gmail.com>
2021-01-14 16:35:57 +0700

    Created project.html and list_projects.html. Created list of links for projects in list_projects.html.

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('about', views.about),
-    path('contacts', views.contacts)
+    path('contacts', views.contacts),
+    path('project/<int:number_of_project>', views.project),
+    path('list_projects', views.list_projects)
 ]
